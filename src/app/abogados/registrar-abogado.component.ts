117703cfcd1a3b5943911b44f5f9126353737950
Hernan Rodriguez <hrodriguezb5@ucentral.edu.co>
2026-05-07 21:47:49 -0500

    feat: HU001 - Crear formulario de registro con problemas

---
+++ src/app/abogados/registrar-abogado.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-registrar-abogado',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './registrar-abogado.component.html'
+})
+export class RegistrarAbogadoComponent {
+  abogado = {
+    nombreCompleto: '',
+    numeroCedula: '',
+    tarjetaProfesional: '',
+    especialidad: '',
+    correoElectronico: '',
+    telefono: ''
+  };
+
+  constructor(private http: HttpClient) {}
+
+  registrar() {
+    this.http.post('http://localhost:8860/abogados', this.abogado)
+      .subscribe({
+        next: () => alert('Abogado registrado exitosamente'),
+        error: (err) => alert('Error: ' + err.message)
+      });
+  }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { RegistrarAbogadoComponent } from './abogados/registrar-abogado.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'abogados/registrar', component: RegistrarAbogadoComponent }
+];
